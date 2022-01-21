b937839573559b3d22fe26c06639dcec8fb535e6
miroslav-opiela <miroslav-opiela@upjs.sk>
2022-01-21 12:15:25 +0100

    2 empty aktivity

--- app/src/main/java/sk/itsovy/android/voting/ResultsActivity.kt
+++ app/src/main/java/sk/itsovy/android/voting/ResultsActivity.kt
@@ -3,9 +3,9 @@ package sk.itsovy.android.voting
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_results)
     }
 }
